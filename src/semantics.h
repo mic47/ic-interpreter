09c058e9b6fa90ec29d3c52b694c1de371b82c47
Michal Nánási <mic@ksp.sk>
2011-09-25 19:19:32 +0200

    added semantics.h

---
+++ src/semantics.h
@@ -0,0 +1,28 @@
+#ifndef SEMANTICS_H
+#define SEMANTICS_H
+
+#include "bytecode.h"
+
+
+#define SEM_V 0
+#define SEM_P 1
+#define SEM_C 2 
+#define SEM_F 3
+#define SEM_L 4
+#define SEM_O 5
+
+struct _Identifier{
+	int type;
+	char *text;
+	int num;
+};
+typedef struct _Identifier Identifier;
+
+struct _Programpiece {
+	int len;
+	int capacity;
+	Instruction *instructions;
+};
+typedef struct _Programpiece Programpiece;
+
+#endif
